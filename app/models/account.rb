94c33e3da0e5965729879b13813118a3aeedbfc7
mattbmoorin <tzaratape@gmail.com>
2021-02-13 22:11:26 -0600

    yml test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ApplicationRecord
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,27 +13,3 @@ class Account < ApplicationRecord
     end
   end 
 end
-    
-    
-    
-    
-    
-  #   .from_omniauth(access_token)
-  #   data = access_token.info
-  #   account = account.where(email: data['email']).first
-
-  #   # Uncomment the section below if you want accounts to be created if they don't exist
-  #   unless account
-  #       account = account.create(
-  #        name: data['name'],
-  #        email: data['email'],
-  #        password: Devise.friendly_token[0,20]
-  #       )
-  #   end
-  #   account
-  # end
-
-  # def self.from_google(email:, full_name:, uid:, avatar_url:)
-  #   return nil unless email =~ /@mybusiness.com\z/
-  #   create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-  # end
