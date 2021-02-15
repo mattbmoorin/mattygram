beb796bec42e9539a20028ad03a6ca09a3017752
mattbmoorin <tzaratape@gmail.com>
2021-02-15 00:20:06 -0600

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,3 @@ Rails.application.routes.draw do
   
   root to: 'public#index'
 end
-
-#devise_for :accounts
-# devise_scope :account do
-#   get 'accounts/sign_in', to: 'accounts/sessions#new', as: :new_accounts_session
-#   get 'accounts/sign_out', to: 'accounts/sessions#destroy', as: :destroy_accounts_session
-# end
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
